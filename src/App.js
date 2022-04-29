e28503915d2befc2b5b88c66ad9b9a073d62cafa
sunnyfur <polin.makarova@mail.ru>
2022-04-29 17:26:58 +0500

    add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className='App'></div>;
-}
+import styles from './assets/styles/Components/page.module.scss';
+
+import Header from './components/Header/Header';
+import Table from './components/Table/Table';
+import Footer from './components/Footer/Footer';
+
+const App = () => (
+  <div className={styles.wrapper}>
+    <Header />
+    <main>
+      <Table />
+    </main>
+    <Footer />
+  </div>
+);
 
 export default App;
