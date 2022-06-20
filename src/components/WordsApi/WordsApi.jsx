ee1a65bc3cad1aaf0234f12fab9842c9620caf35
sunnyfur <polin.makarova@mail.ru>
2022-06-20 14:56:53 +0500

    feat: change path

--- src/components/WordsApi/WordsApi.jsx
+++ src/components/WordsApi/WordsApi.jsx
@@ -4,7 +4,7 @@ import Loader from '../Loader/Loader';
 export const WordsContext = createContext();
 
 const getWords = () =>
-  fetch('https://itgirlschool.justmakeit.ru/api/words')
+  fetch('//itgirlschool.justmakeit.ru/api/words')
     .then((response) => {
       if (response.ok) {
         // Проверяем что код ответа 200
